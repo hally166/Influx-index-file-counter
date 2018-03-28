e1d506be1f785a374cc67d05c7837bd0eacb3ca7
Sanger Institute Flow Cytometry <ch15@sanger.ac.uk>
2018-03-28 15:39:53 +0100

    Shortened pre-amble

--- INFLUX index file counter v1.0.py
+++ INFLUX index file counter v1.0.py
@@ -1,14 +1,8 @@
-#Copyright (c) 2017 Genome Research Ltd.
-
 #Index file counter
 #v1.0 Jun 2017
 #Python 3.5 (2016)
-#Author : Christopher Hall, Wellcome Trust Sanger Institute, christopher.hall@sanger.ac.uk
-
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+#Author : Christopher Hall, Wellcome Sanger Institute, christopher.hall@sanger.ac.uk
+#License : GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 
 #Counts all the index files in it's folder and subfolders and displays the statisitics.  
 #We orgainise our files using one subfolder per user and another subfolder per experiment.  This script uses this routine to count the number of experiments.  i.e. User1\01jan2017\file.fcs
